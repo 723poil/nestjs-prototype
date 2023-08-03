10823a60d91cff97a8b5c3adf4dfd95e0ae78a0c
723poil <leetkdguq73@naver.com>
2023-08-03 10:39:40 +0900

    Use async for typeorm linking to database

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # 프로토타입
 
 ## 적용 기능
-+ Swagger [스웨거 접속 경로](http://localhost:3000/swagger), [참고](https://velog.io/@inmyblue0930/NestJs-Swagger%EC%97%B0%EA%B2%B0-%EB%B3%B4%EC%95%88%EA%B4%80%EB%A6%AC)
-+ Typeorm [참고](https://develop-const.tistory.com/19)
++ Swagger
+    [스웨거 접속 경로](http://localhost:3000/swagger)<br>
+    [[NestJs] Swagger연결 + 보안관리](https://velog.io/@inmyblue0930/NestJs-Swagger%EC%97%B0%EA%B2%B0-%EB%B3%B4%EC%95%88%EA%B4%80%EB%A6%AC)
++ Typeorm <br>
+    [NestJs 데이터베이스 연동하기 (mysql, typeorm)](https://develop-const.tistory.com/19)<br>
+    [NestJS | 데이터베이스 연결, 설정 정보를 입력하는 다양한 방법... (database connection)](https://gaemi606.tistory.com/entry/NestJS-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4-%EC%97%B0%EA%B2%B0-%EC%84%A4%EC%A0%95-%EC%A0%95%EB%B3%B4%EB%A5%BC-%EC%9E%85%EB%A0%A5%ED%95%98%EB%8A%94-%EB%8B%A4%EC%96%91%ED%95%9C-%EB%B0%A9%EB%B2%95-database-connection)
 
 ## request 규칙
 Get이 아닌 API들이 request body를 사용할 경우 -> DTO 사용
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
-import { Book } from './books/entities/book.entity';
+import { ConfigModule } from '@nestjs/config';
+import { MySqlConfigModule } from './config/database/config.module';
+import { MySqlConfigService } from './config/database/config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1', // localhost로 하면 오류발생함
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      synchronize: true, // 개발단계에서만 사용
-      logging: true,     // 콘솔에 로그 출력
-      entities: [Book],  // 사용할 엔티티 추가해줘야함
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
     }),
     BooksModule
   ],
